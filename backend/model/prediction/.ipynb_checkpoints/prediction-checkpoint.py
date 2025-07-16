f102f15e2e7e41f139bd6cdf7990b5ec49962fa1
Marti Sans <martimasover@gmail.com>
2025-07-16 19:51:03 +0200

    Work in progress: To see if main is update

---
+++ backend/model/prediction/.ipynb_checkpoints/prediction-checkpoint.py
@@ -0,0 +1,20 @@
+
+import pickle
+import os
+
+#Takes 3 inputs: age, minutes, goals
+def predict_player_value(age: float, minutes_played: float, goals: float):
+    """
+    Load trained model and return value prediction.
+    """
+    model_path = os.path.join(os.path.dirname(__file__), '..', 'model', 'model.pkl')
+
+    with open(model_path, 'rb') as f:
+        model = pickle.load(f)
+
+    #Makes a prediction
+    X_pred = [[age, minutes_played, goals]]
+    prediction = model.predict(X_pred)
+
+    #Returns a single value (the predicted transfer value)
+    return prediction[0]
