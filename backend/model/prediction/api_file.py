0e5353cfa41a221632405de5d46f7c7d78258da2
Joao Miranda <103436082+Kamiranda@users.noreply.github.com>
2025-07-22 22:03:26 +0100

    changes backend

--- backend/model/prediction/api_file.py
+++ backend/model/prediction/api_file.py
@@ -1,13 +1,25 @@
-from fastapi import FastAPI
-from model.prediction.prediction import predict_player_value
 
-app = FastAPI()
+import pickle
+import os
 
-@app.get("/")
-def root():
-    return {"message": "the football scout is ready to go!"}
+#Takes 3 inputs: age, minutes, goals
+def predict_player_value(age: float, minutes_played: float, goals: float, assists: float, position: str):
 
-@app.get("/predict")
-def predict(age: float, minutes_played: float, goals: float):
-    prediction = predict_player_value(age, minutes_played, goals)
-    return {"predicted_value": prediction}
+    """
+    Load trained model and return value prediction.
+    """
+    model_path = os.path.join(
+        os.path.dirname(__file__),
+        '..',
+        'stjin_model.pkl'
+    )
+
+    with open(model_path, 'rb') as f:
+        model = pickle.load(f)
+
+    #Makes a prediction
+    X_pred = [[age, minutes_played, goals, assists, position]]
+    prediction = model.predict(X_pred)
+
+    #Returns a single value (the predicted transfer value)
+    return prediction[0]
